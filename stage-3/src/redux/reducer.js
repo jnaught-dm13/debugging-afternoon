898db173c2d4e7efad6f95a2d27ca77a5f4fe4d9
Aaron.Nacua <alnacua@gmail.com>
2018-04-11 14:54:04 -0500

    fixed the final bug that was hiding like a cockroach

--- stage-3/src/redux/reducer.js
+++ stage-3/src/redux/reducer.js
@@ -16,7 +16,9 @@ let initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case GET_ALL_PRODUCTS_PENDING:
-      return Object.assign({}, state, { loading: true });
+      return Object.assign({}, state, {
+        loading: true
+      });
 
     case GET_ALL_PRODUCTS_FULFILLED:
       return Object.assign({}, state, {
@@ -31,8 +33,10 @@ export default function reducer(state = initialState, action) {
 
     case REMOVE_FROM_SHOPPING_CART:
       let newArray = state.shoppingCart.slice();
-      newArray.splice(action.index, 1);
-      return Object.assign({}, { shoppingCart: newArray });
+      newArray.splice(action.payload, 1);
+      return Object.assign({}, state, {
+        shoppingCart: newArray
+      });
 
     default:
       return state;
